3a20e723a1236946b2e99742d4b85af8e274a640
HenrryHernandez <jenrruhndz@gmail.com>
2024-02-29 15:46:39 -0600

    feat (user): create endpoint to update the user information

--- api/src/controllers/user.js
+++ api/src/controllers/user.js
@@ -17,3 +17,38 @@ export const userInformation = async (req = request, res = response) => {
 
   res.status(200).json({ msg: "", data: user, success: true });
 };
+
+export const updateUserInformation = async (req = request, res = response) => {
+  const { id } = req.params;
+  const body = req.body;
+
+  // here we are removing the data the user (in my opinion) should not be
+  // able to change and leaving the data allowed to change in "...data"
+  const { _id, guid, isActive, email, ...data } = body;
+
+  const db = getConnection();
+
+  const user = db.data.users.find((user) => user._id === id);
+
+  if (!user) {
+    return res
+      .status(404)
+      .json({ msg: "User not found", data: null, success: false });
+  }
+
+  const modifiedUser = { ...user, ...data };
+
+  const users = db.data.users.map((user) =>
+    user._id === id ? modifiedUser : user
+  );
+
+  db.data = {
+    users,
+  };
+
+  db.write();
+
+  res
+    .status(200)
+    .json({ msg: "User modified successfully!", data: null, success: true });
+};
--- api/src/routes/user.js
+++ api/src/routes/user.js
@@ -1,7 +1,8 @@
 import { Router } from "express";
 
-import { userInformation } from "../controllers/user.js";
+import { updateUserInformation, userInformation } from "../controllers/user.js";
 
 export const UserRoute = Router();
 
 UserRoute.get("/:id", [], userInformation);
+UserRoute.put("/:id", [], updateUserInformation);
