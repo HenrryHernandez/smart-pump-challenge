1e9ecaf08272d1f8c1d91805ad33156cbd16a36e
HenrryHernandez <jenrruhndz@gmail.com>
2024-03-02 11:32:46 -0600

    style (landing-page): add components to the page

--- client/smart-pump-client/src/app/page.tsx
+++ client/smart-pump-client/src/app/page.tsx
@@ -1,3 +1,30 @@
+import { PageWrapper } from "@/components/PageWrapper";
+import { Banner } from "@/components/landing-page/Banner";
+import { AboutUs } from "@/components/landing-page/AboutUs";
+import { FillerCards } from "@/components/landing-page/FillerCards";
+import { Services } from "@/components/landing-page/Services";
+import { HowItWorks } from "@/components/landing-page/HowItWorks";
+import { Pricing } from "@/components/landing-page/Pricing";
+import { Footer } from "@/components/landing-page/Footer";
+
 export default function Home() {
-  return <main></main>;
+  return (
+    <PageWrapper className="p-0">
+      <main className="w-full flex flex-col center relative landing-bg">
+        <Banner />
+
+        <AboutUs />
+
+        <FillerCards />
+
+        <Services />
+
+        <HowItWorks />
+
+        <Pricing />
+
+        <Footer />
+      </main>
+    </PageWrapper>
+  );
 }
