04ef76c2f986576b4df32e1c28b22ccc9023c702
HenrryHernandez <jenrruhndz@gmail.com>
2024-03-02 11:31:48 -0600

    style (landing-page): create HowItWorks component

---
+++ client/smart-pump-client/src/components/landing-page/HowItWorks.tsx
@@ -0,0 +1,21 @@
+export const HowItWorks = () => {
+  return (
+    <div className="min-h-[350px] w-full flex flex-col center px-12 py-40 sm:px-20 overflow-hidden bg-blue-200">
+      <div className="max-w-[1024px] flex flex-col center gap-y-8 lg:p-8 relative">
+        <h2 className="text-5xl text-center font-rum-raisin text-blue-500">
+          How it works
+        </h2>
+
+        <p className="text-center font-duru-sans">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum.
+        </p>
+      </div>
+    </div>
+  );
+};
